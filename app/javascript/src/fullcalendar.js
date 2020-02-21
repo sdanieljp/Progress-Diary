d0b4397fcbc93c94fa7046590b7de5943642565e
Dan Sato <sdanieljp@yahoo.co.jp>
2020-02-21 22:30:17 +0900

    カレンダー表示改良

--- app/javascript/src/fullcalendar.js
+++ app/javascript/src/fullcalendar.js
@@ -1,11 +1,28 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin ]
+    plugins: [ dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin ],
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+    },
+    navLinks: true, // can click day/week names to navigate views
+    businessHours: true, // display business hours
+    editable: true,
+    events: [
+        {
+          title: 'Business Lunch',
+          start: '2020-02-03T13:00:00',
+        }
+    ]
   });
 
   calendar.render();
